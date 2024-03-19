b3a1da4944f685f99a9f3d10ec0ccff4ca4195a4
Feng Yue <fengyue729@gmail.com>
2024-03-19 17:07:45 +0800

    'Validation!'

--- test/system/products_test.rb
+++ test/system/products_test.rb
@@ -1,49 +1,34 @@
 require "application_system_test_case"
 
-class ProductsTest < ApplicationSystemTestCase
-  setup do
-    @product = products(:one)
+class ProductTest < ActiveSupport::TestCase
+  fixtures :products
+
+end
+
+def create
+  @product = Product.new(product_params)
+  respond_to do |format|
+    if @product.save
+      format.html { redirect_to @product,
+        notice: "Product was successfully created." }
+      format.json { render :show, status: :created,
+        location: @product }
+    else
+      puts @product.errors.full_messages
+      format.html { render :new,
+        status: :unprocessable_entity }
+      format.json { render json: @product.errors,
+        status: :unprocessable_entity }
+    end
   end
+end
 
-  test "visiting the index" do
-    visit products_url
-    assert_selector "h1", text: "Products"
-  end
-
-  test "should create product" do
-    visit products_url
-    click_on "New product"
-
-    fill_in "\", with: @product.\
-    fill_in "Description", with: @product.description
-    fill_in "Image url", with: @product.image_url
-    fill_in "Price", with: @product.price
-    fill_in "Title", with: @product.title
-    click_on "Create Product"
-
-    assert_text "Product was successfully created"
-    click_on "Back"
-  end
-
-  test "should update Product" do
-    visit product_url(@product)
-    click_on "Edit this product", match: :first
-
-    fill_in "\", with: @product.\
-    fill_in "Description", with: @product.description
-    fill_in "Image url", with: @product.image_url
-    fill_in "Price", with: @product.price
-    fill_in "Title", with: @product.title
-    click_on "Update Product"
-
-    assert_text "Product was successfully updated"
-    click_on "Back"
-  end
-
-  test "should destroy Product" do
-    visit product_url(@product)
-    click_on "Destroy this product", match: :first
-
-    assert_text "Product was successfully destroyed"
-  end
+test "product is not valid without a unique title - i18n" do
+  product = Product.new(title: products(:ruby).title,
+                        description: "yyy",
+                        price: 1,
+                        image_url: "fred.gif")
+  assert product.invalid?
+  assert_equal [I18n.translate('errors.messages.taken')],
+               product.errors[:title]
 end
